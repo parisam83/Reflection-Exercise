7ebe54dadf525c3a27ab4fb787e39dd6e31cdf62
Parisa <mousaviparisa5@yahoo.com>
2023-07-05 09:04:55 +0330

    Load class and search for name of fields in 'values' map

--- src/ir/sharif/math/ap2023/hw7/ObjectMaker.java
+++ src/ir/sharif/math/ap2023/hw7/ObjectMaker.java
@@ -1,6 +1,9 @@
 package ir.sharif.math.ap2023.hw7;
 
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.*;
 
@@ -21,7 +24,20 @@ public class ObjectMaker {
     }
 
     public Object makeObject(Map<String, Object> values, String className) throws ReflectiveOperationException {
-        // TODO
+        ClassLoader classLoader = ObjectMaker.class.getClassLoader();
+        Class givenClass = classLoader.loadClass(className);
+        Field[] fields = givenClass.getDeclaredFields();
+        Method[] methods = givenClass.getDeclaredMethods();
+
+        Constructor defaultConstructor = givenClass.getDeclaredConstructor();
+        defaultConstructor.setAccessible(true);
+        Object object = defaultConstructor.newInstance();
+
+        for (Field field : fields){
+            field.setAccessible(true);
+            Object value = values.get(field.getName());
+            if (value != null) field.set(object, value);
+        }
         return null;
     }
 
