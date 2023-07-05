45391858af85efb14748639179ba279846a04ebf
Parisa <mousaviparisa5@yahoo.com>
2023-07-05 09:14:16 +0330

    Check if value of the specific key in the map is List or not

--- src/ir/sharif/math/ap2023/hw7/ObjectMaker.java
+++ src/ir/sharif/math/ap2023/hw7/ObjectMaker.java
@@ -1,6 +1,7 @@
 package ir.sharif.math.ap2023.hw7;
 
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -35,13 +36,23 @@ public class ObjectMaker {
 
         for (Field field : fields){
             field.setAccessible(true);
-            Object value = null;
+            Object value;
 
             // Check if @Name is used and find the name of the field in the given map
             if (field.getDeclaredAnnotation(Name.class) == null) value = values.get(field.getName());
             else value = values.get(field.getDeclaredAnnotation(Name.class).name());
 
-            if (value != null) field.set(object, value);
+            if (value != null) {
+                // Check if value is a List of not
+                if (value instanceof List<?>){
+                    Object valueToSet = Array.newInstance(field.getType().getComponentType(), ((List<?>) value).size());
+                    for (int i = 0; i < ((List<?>) value).size(); i++)
+                        Array.set(valueToSet, i, ((List<?>) value).get(i));
+                    field.set(object, valueToSet);
+                }
+                else
+                    field.set(object, value);
+            }
         }
         return null;
     }
